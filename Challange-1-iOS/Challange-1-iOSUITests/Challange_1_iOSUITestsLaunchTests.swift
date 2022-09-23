66aa0ec6394fdbf8be226c98c1462f4c9c85b45f
Daniel Silva <daniel.silva20@hotmail.com>
2022-09-23 11:33:03 +0100

    starter project

---
+++ Challange-1-iOS/Challange-1-iOSUITests/Challange_1_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Challange_1_iOSUITestsLaunchTests.swift
+//  Challange-1-iOSUITests
+//
+//  Created by Daniel Silva on 9/23/22.
+//
+
+import XCTest
+
+class Challange_1_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
